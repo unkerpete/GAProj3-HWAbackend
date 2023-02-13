d6b429ee9a4928f8b4a35f8c7cc419a89dba9b87
unkerpete <peterkjx@gmail.com>
2023-02-13 11:45:32 +0800

    Removed duplicate model folder

--- models/events.js
+++ models/events.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const EventsSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    date: { type: Date, required: true },
+    dateStart: { type: Date, required: true },
     dateEnd: { type: Date },
-    start: { type: Date, required: true },
-    end: { type: Date },
     description: { type: String, required: true },
     img: { type: String }, // TODO: modify to binary format
     action: { type: String },
